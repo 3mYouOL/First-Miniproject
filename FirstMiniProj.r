6482a366d069b85effdd79b4f4bd5b088bb56d30
3mYouOL <3mYouOL@gmail.com>
2021-10-21 17:48:35 +0800

    Update Miniproject files - Clean unwanted outputs - Add `corr` function - Update Jupyter notebook with `corr` test cases

--- FirstMiniProj.r
+++ FirstMiniProj.r
@@ -20,10 +20,24 @@ complete <- function(directory, id = 1:332) {
     for (csv_idx in id) {
        csv_clean <- na.omit(read.csv(paste(directory, "/", csv_files[csv_idx], sep="")))
        csv_cumulative[csv_idx] <- nrow(csv_clean)
-       print(csv_idx)
     }
 
-    print(csv_cumulative)
     clean_data <- data.frame(id = id, nobs = na.omit(csv_cumulative))
     clean_data
+}
+
+corr <- function(directory, threshold = 0) {
+    csv_files <- list.files(path = directory)[1:332]
+    csv_correlations <- vector(mode = "numeric")
+
+    for (csv_file in csv_files) {
+        csv_clean <- na.omit(read.csv(paste(directory, "/", csv_file, sep="")))
+
+        if(nrow(csv_clean) > threshold) {
+            csv_correlation <- cor(csv_clean[, "sulfate"], csv_clean[, "nitrate"])
+            csv_correlations <- append(csv_correlations, csv_correlation)
+        }
+    }
+
+    csv_correlations
 }
