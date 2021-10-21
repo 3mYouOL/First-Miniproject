6ecfa82637bf1a349919c14905b8eb3b78bc0b72
3mYouOL <3mYouOL@gmail.com>
2021-10-21 16:18:05 +0800

    Update Miniproject files - Fix bug in reading directory files - Add `complete` function - Update Jupyter notebook with `complete` algorithm test cases

--- FirstMiniProj.r
+++ FirstMiniProj.r
@@ -5,10 +5,25 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     csv_read <- vector(mode = "list")
 
     for (csv_file in csv_files) {
-       csv_read <- append(csv_read, list(read.csv(csv_file)))
+       csv_read <- append(csv_read, list(read.csv(paste(directory, "/", csv_file, sep=""))))
     }
 
     csv_cumulative <- rbind.fill(csv_read)
 
     mean(csv_cumulative[, pollutant], na.rm = TRUE)
+}
+
+complete <- function(directory, id = 1:332) {
+    csv_files <- list.files(path = directory)[1:332]
+    csv_cumulative <- c()
+
+    for (csv_idx in id) {
+       csv_clean <- na.omit(read.csv(paste(directory, "/", csv_files[csv_idx], sep="")))
+       csv_cumulative[csv_idx] <- nrow(csv_clean)
+       print(csv_idx)
+    }
+
+    print(csv_cumulative)
+    clean_data <- data.frame(id = id, nobs = na.omit(csv_cumulative))
+    clean_data
 }
